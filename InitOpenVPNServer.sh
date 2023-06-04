930a10f5a46420eb2b55198b6d114936099f038c
itex77 <111645014+itex77@users.noreply.github.com>
2023-06-04 10:38:41 +0300

    Update InitOpenVPNServer.sh

--- InitOpenVPNServer.sh
+++ InitOpenVPNServer.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+mkdir -p /etc/openvpn/keys
 mkdir /etc/openvpn/easy-rsa
-cp -R /usr/share/easy-rsa /etc/openvpn/
 cd /etc/openvpn/easy-rsa/
+cp -r /usr/share/easy-rsa/* .
+
+sudo bash -c 'cat > /etc/openvpn/easy-rsa/vars <<EOF
+export KEY_COUNTRY="RU"
+export KEY_PROVINCE="Tatarstan"
+export KEY_CITY="Kazan"
+export KEY_ORG="SKILLFACTORY"
+export KEY_EMAIL="info@corp.org"
+export KEY_OU="corp"
+EOF'
+
 ./easyrsa init-pki
 ./easyrsa build-ca
 
-ServerName=zabbix
-DomainName=sf-admin-gusevnv.local
-VPNNetwork='10.1.1.0 255.255.255.0'
-NameServer=10.1.1.6
+ServerName=ub1srv
+VPNNetwork='10.8.0.0 255.255.255.0'
+
 ./easyrsa gen-dh
 
 ./easyrsa build-server-full $ServerName nopass
@@ -23,33 +33,27 @@ bash -c "cat > /etc/openvpn/$ServerName.conf <<EOF
 port 1194
 proto tcp
 dev tun
-ca ca.crt
-cert $ServerName.crt
-key $ServerName.key
-dh dh.pem
-ifconfig-pool-persist ipp.txt
+ca keys/ca.crt
+cert keys/$ServerName.crt
+key keys/$ServerName.key
+dh keys/dh.pem
 server $VPNNetwork
-push \"dhcp-option DNS $NameServer\"
-push \"dhcp-option DOMAIN $DomainName\"
+ifconfig-pool-persist ipp.txt
 keepalive 10 120
-cipher DES-CBC
 persist-key
 persist-tun
 script-security 2
-client-connect "/etc/openvpn/setdns.sh"
-client-to-client
-status /var/log/openvpn/$ServerName.log
-EOF"
+status /var/log/openvpn/openvpn-status.log
+log-append /var/log/openvpn/openvpn.log
+cipher DES-CBC
+EOF'
 
-cd
-cp setdns.sh /etc/openvpn/
-chmod +x /etc/openvpn/setdns.sh
+mkdir /var/log/openvpn
 
 systemctl start openvpn@$ServerName
 systemctl enable openvpn@$ServerName
 
 unset ServerName
-unset DomainName
 unset VPNNetwork
-unset NameServer
+
 
